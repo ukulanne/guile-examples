c4b5bc4180c4ade1112697c6cc0ad594c265eb44
Anne Summers <ukulanne@gmail.com>
2019-03-08 22:08:45 -0600

    Update client.scm

--- web/client.scm
+++ web/client.scm
@@ -1,3 +1,30 @@
+#!/usr/bin/env guile -s
+!#
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Anne Summers                  ;;
+;; <ukulanne@gmail.com>          ;;
+;; March 8, 2019                 ;;
+;; Guile web app server example  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;    This program is free software: you can redistribute it and/or modify  ;;
+;;    it under the terms of the GNU General Public License as published by  ;;
+;;    the Free Software Foundation, either version 3 of the License, or     ;;
+;;    (at your option) any later version.                                   ;;
+;;                                                                          ;;
+;;    This program is distributed in the hope that it will be useful,       ;;
+;;    but WITHOUT ANY WARRANTY; without even the implied warranty of        ;;
+;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ;;
+;;    GNU General Public License for more details.                          ;;
+;;                                                                          ;;
+;;    You should have received a copy of the GNU General Public License     ;;
+;;    along with this program.  If not, see <https://www.gnu.org/licenses/>.;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Time-stamp: <2019-03-08 14:55:40 panda> 
+
 (use-modules (web uri)
 	     (web client))
 
